20f59d20091b14fe0380285640ff0b64983b7589
Nitin225 <177527946+Nitin225@users.noreply.github.com>
2026-04-13 11:04:11 +0000

    feat: parse input into args

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include<iostream>
 #include<string>
+#include <sstream>
+#include <vector>
 using namespace std;
 
 int main(){
     while(true){
         string input;
+        string token;
+        vector<string> args;
         cout<< "shell> ";
         getline(cin, input);
+        istringstream iss(input);
+
+        while(iss >> token){
+            args.push_back(token);
+        }
+
         if (input == "exit") break;
-        cout << input << endl;
+        cout << "Command: " << args[0] << endl;
+        cout << "Args count: " << args.size() << endl;
+
+
     }
+
+
 }
