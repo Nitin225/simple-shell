1752903ec6ccb4948b3486ea501e4e42cd073169
Nitin225 <177527946+Nitin225@users.noreply.github.com>
2026-04-17 17:58:29 +0000

    feat: output redirection using dup2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include<vector>
 #include<unistd.h>
 #include<sys/wait.h>
+#include <fcntl.h>
 #include<cstring>
 using namespace std;
 
@@ -27,20 +28,38 @@ int main(){
         }
 
         if (args[0] == "exit") break;
+
+        string outfile = "";
+        vector<string> cmd_args;
+
+        for (int i = 0; i < args.size(); i++) {
+            if (args[i] == ">") {
+                outfile = args[i+1];
+                break;
+            }
+            cmd_args.push_back(args[i]);
+        }
+
         pid_t pip = fork();
         if (pip == 0) {
+            if (!outfile.empty()) {
+            int fd = open(outfile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            }
             vector<char*> cargs;
-            for (auto& arg : args) {
+            for (auto& arg : cmd_args) {
                 cargs.push_back(&arg[0]);
             }
-            cargs.push_back(nullptr); // array ka end
+            cargs.push_back(nullptr); 
             execvp(cargs[0], cargs.data());
             cerr << "Command invalid: " << args[0] << endl;
-            exit(1);  
+            exit(1);
         }
         else {
             wait(nullptr);
         }
+
 
     }
 
