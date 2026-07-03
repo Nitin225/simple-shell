8faae56f7a3b7e55586b18fd8897452384d9a7b4
Nitin225 <177527946+Nitin225@users.noreply.github.com>
2026-07-03 13:42:16 +0000

    Complete initial Unix shell implementation

--- main.cpp
+++ main.cpp
@@ -36,6 +36,27 @@ void builtinPwd() {
     }
 }
 
+
+vector<vector<string>> splitByPipe(const vector<string>& args) {
+    vector<vector<string>> result;      
+    vector<string> current;            
+
+    for (int i = 0; i < args.size(); i++) {
+        if (args[i] == "|") {
+            result.push_back(current);
+            current.clear();
+        } else {
+            current.push_back(args[i]);
+        }
+    }
+
+    result.push_back(current);
+
+    return result;
+}
+
+
+
 int main(){
     while(true){
         string input;
@@ -50,6 +71,9 @@ int main(){
         }
         if (args.empty()) continue;
 
+        
+        
+            
         if (args[0] == "cd") {
             builtinCd(args);
             continue;
@@ -68,74 +92,114 @@ int main(){
             exit(code);
         }
 
+
+        string infile = "";
+        bool appendMode = false;
         string outfile = "";
         vector<string> cmd_args;
 
         for (int i = 0; i < args.size(); i++) {
-            if (args[i] == ">") {
+            if (args[i] == ">" || args[i] == ">>") {
+                appendMode = (args[i] == ">>");
+                if (i + 1 >= args.size()) {
+                    cerr << "shell: syntax error: expected filename after " << args[i] << "\n";
+                    continue;
+                }
                 outfile = args[i+1];
-                break;
+                i++;             
+                continue;
+            }
+            if (args[i] == "<") {
+                if (i + 1 >= args.size()) {
+                    cerr << "shell: syntax error: expected filename after " << args[i] << "\n";
+                    continue;
+                }
+                infile = args[i+1];
+                i++;             
+                continue;
             }
             cmd_args.push_back(args[i]);
         }
 
-        string pipe_cmd = "";
-        vector<string> left_args, right_args;
-        bool has_pipe = false;
 
-        for (int i = 0; i < args.size(); i++) {
-            if (args[i] == "|") {
-                has_pipe = true;
-                for (int j = i+1; j < args.size(); j++) {
-                    right_args.push_back(args[j]);
+        vector<vector<string>> commands = splitByPipe(args);
+        if(commands.size() > 1){
+            int n = commands.size();
+            vector<vector<int>> pipes(n-1, vector<int>(2));
+            for(int i=0; i<n-1; i++){
+                if(pipe(pipes[i].data()) < 0){
+                    perror("pipes");
+                    exit(1);
                 }
-                break;
             }
-            left_args.push_back(args[i]);
+            vector<pid_t> pids;
+
+            for (int i = 0; i < n; i++) {
+                pid_t pid = fork();
+                if (pid == 0) {
+                
+                    if (i > 0) {
+                        dup2(pipes[i-1][0], STDIN_FILENO);
+                    }
+                    if (i < n - 1) {
+                        dup2(pipes[i][1], STDOUT_FILENO);
+                    }
+                    for (int j = 0; j < n - 1; j++) {
+                        close(pipes[j][0]);
+                        close(pipes[j][1]);
+                    }
+                    vector<char*> cargs;
+                    for (auto& arg : commands[i]) cargs.push_back(&arg[0]);
+                    cargs.push_back(nullptr);
+                    execvp(cargs[0], cargs.data());
+
+                    cerr << "shell: command not found: " << commands[i][0] << "\n";
+                    exit(1);
+                }
+                pids.push_back(pid);
+            }
+            for (int j = 0; j < n - 1; j++) {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+
+            for (int j = 0; j < n; j++) {
+                waitpid(pids[j], nullptr, 0);
+            }
+            continue;
         }
 
-        if (has_pipe) {
-        int pipefd[2];
-        pipe(pipefd);
-        
-        pid_t left = fork();
-        if (left == 0) {
-            dup2(pipefd[1], STDOUT_FILENO); 
-            close(pipefd[0]);
-            close(pipefd[1]);
-            vector<char*> cargs;
-            for (auto& arg : left_args) cargs.push_back(&arg[0]);
-            cargs.push_back(nullptr);
-            execvp(cargs[0], cargs.data());
-            exit(1);
+
+
+
+        if(cmd_args.empty()){
+            cerr << "shell: syntax error: no command given\n";
+            continue;
         }
-        
-        pid_t right = fork();
-        if (right == 0) {
-            dup2(pipefd[0], STDIN_FILENO); 
-            close(pipefd[0]);
-            close(pipefd[1]);
-            vector<char*> cargs;
-            for (auto& arg : right_args) cargs.push_back(&arg[0]);
-            cargs.push_back(nullptr);
-            execvp(cargs[0], cargs.data());
-            exit(1);
-        }
-        
-        close(pipefd[0]);
-        close(pipefd[1]);
-        wait(nullptr);
-        wait(nullptr);
-        continue;
-    }
 
         pid_t pip = fork();
         if (pip == 0) {
-            if (!outfile.empty()) {
-            int fd = open(outfile.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
+            if (!infile.empty()) {
+                int fdIn = open(infile.c_str(), O_RDONLY);
+                if (fdIn < 0) {
+                    cerr << "shell: " << infile << ": No such file or directory\n";
+                    exit(1);
+                }
+                dup2(fdIn, STDIN_FILENO);
+                close(fdIn);
             }
+
+            if (!outfile.empty()) {
+                int flags = O_WRONLY | O_CREAT | (appendMode ? O_APPEND : O_TRUNC);
+                int fd = open(outfile.c_str(), flags, 0644);
+                if (fd < 0) {
+                    cerr << "shell: cannot open " << outfile << ": for writings\n";
+                    exit(1);
+                }
+                dup2(fd, STDOUT_FILENO);
+                close(fd);
+            }
+
             vector<char*> cargs;
             for (auto& arg : cmd_args) {
                 cargs.push_back(&arg[0]);
@@ -148,9 +212,5 @@ int main(){
         else {
             wait(nullptr);
         }
-
-
     }
-
-
 }
